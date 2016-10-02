76603d2996f87526b3fb21df32adcfc92a7acd2c
Margaret Yu <margyu@mit.edu>
2016-10-02 00:45:33 -0400

    fixed click functionality when a van stop is clicked

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ $(document).ready(function() {
 	      mapTypeId: 'roadmap'
 	    };
 
-	    map = new google.maps.Map(document.getElementById('map'),
-	        mapOptions);
 	    map = L.map('map').setView([42.292, -71.3059], 16);
 		L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar'}).addTo(map);	    
 	}
@@ -20,21 +18,26 @@ $(document).ready(function() {
 		    type: "GET",
 		    url: './buildings_wgs84.json',
 		    dataType: 'json',
-		    success: function (data) {
-				//geojsonLayer = L.geoJson(data, {onEachFeature: attachPopup}).addTo(map);		    	
+		    success: function (data) {		    	
 		    	buildings = data.features;
 		    	for (var building in buildings) {
 		    		newBuilding = {};
 		    		newBuilding.title = buildings[building].properties["NAME"];
 		    		newBuilding.outline = buildings[building].geometry.coordinates;
-		    		newBuilding.marker = new L.geoJson(buildings[building], {onEachFeature: attachPopup});
+		    		newBuilding.marker = new L.geoJson(buildings[building], {color: "#e1ba40", weight: 2, onEachFeature: attachPopup});
+		    		newBuilding.marker.on('mouseover', function(e) {
+		    			this.openPopup();
+		    		});
+				  	newBuilding.marker.on('mouseout', function(e) {
+				  		this.invoke('closePopup');
+				  	});		    		
 		    		newBuilding.marker.addTo(map);
 		    		campusBuildings.push(newBuilding);
 		    	}		    	
 		    }
 		});	
 	    function attachPopup(feature, layer) {
-	        layer.bindPopup(feature.properties["NAME"]);
+	        layer.bindPopup("<strong>BUILDING</strong><br>" + feature.properties["NAME"]);
 	    }		
 	}
 
@@ -48,18 +51,19 @@ $(document).ready(function() {
 		    success: function (data) {		    	
 		    	vanstops = data.features;
 		    	for (var vanstop in vanstops) {
-		    		//L.geoJson(vanstops[vanstop]).addTo(map);
 		    		newVanStop = {};
 		    		newVanStop.title = vanstops[vanstop].properties.name;
 		    		newVanStop.outline = vanstops[vanstop].geometry.coordinates.reverse();
 		    		newVanStop.times = [vanstops[vanstop].properties.timeEarly, vanstops[vanstop].properties.timeLate];
-		    		newVanStop.estTime = 3;
+		    		newVanStop.loc = vanstops[vanstop].properties.loc;
 		    		newVanStop.picture = vanstops[vanstop].properties.picture;
-		    		newVanStop.marker = new L.circleMarker(newVanStop.outline, {color: 'purple', fillColor: 'purple', radius: 10, weight: 5, opacity: 1}).bindPopup(newVanStop.title + " - " + vanstops[vanstop].properties.loc);
+		    		newVanStop.marker = new L.circleMarker(newVanStop.outline, {color: '#ad3ccc', fillColor: '#ad3ccc', radius: 12, weight: 12}).bindPopup("<strong>VAN STOP</strong><br>" + newVanStop.title + "<br>" + vanstops[vanstop].properties.loc);
 				  	newVanStop.marker.on('mouseover', function(e) {
+				  		this.setRadius(20);
 				  		this.openPopup();
 				  	});
 				  	newVanStop.marker.on('mouseout', function(e) {
+				  		this.setRadius(10);
 				  		this.closePopup();
 				  	});
 				  	newVanStop.marker.on('click', function(e) {
@@ -68,56 +72,46 @@ $(document).ready(function() {
 				  			if (e.target == allVanStopMarkers[marker]) {
 				  				vanstop = campusVanStops[marker];
 				  				name = vanstop.title;
-				  				timeEarly = vanstop.times[0];
+				  				timeEarly = vanstop.times[0][1];
 				  				estimate = vanstop.estTime;
 				  				img = vanstop.picture;
-				  				infoToDisplay = "<h1>Wellesley College Access Van</h1>";
-				  				infoToDisplay += "<div><p class='userp'>" + name + "</p>";
+				  				infoToDisplay = "<p id='display' style='font-weight: bold; color: #126180; font-size: 20px; margin-bottom: 15px'><span style='font-size: 24px'>WELLESLEY COLLEGE</span> <br> ACCESS VAN </p>"
+				  				infoToDisplay += "<div id='info'><p style='font-size: 18px; color: #126180; margin-bottom: 10px'>Info for Van Stop:</p><p><strong>" + name + "</strong><br><em>" + newVanStop.loc + "</em></p>";
 				  				if (img.length > 0) {
-				  					infoToDisplay += "<img src='" + img + "'/>"; 
+				  					infoToDisplay += "<img class='pic' src='" + img + "'/>"; 
 				  				}
 				  				randomTime = Math.floor(Math.random(1)*16);
 				  				dictOfTimes = {
-				  					0: ["06", convertToTwoDigits(String(timeEarly[1])), "PM"],
-				  					1: ["06", String(timeEarly[1]+30), "PM"],
-				  					2: ["07", convertToTwoDigits(String(timeEarly[1])), "PM"],
-				  					3: ["07", String(timeEarly[1]+30), "PM"],
-				  					4: ["08", convertToTwoDigits(String(timeEarly[1])), "PM"],
-				  					5: ["08", String(timeEarly[1]+30), "PM"],
-				  					6: ["09", convertToTwoDigits(String(timeEarly[1])), "PM"],
-				  					7: ["09", String(timeEarly[1]+30), "PM"],
-				  					8: ["10", convertToTwoDigits(String(timeEarly[1])), "PM"],
-				  					9: ["10", String(timeEarly[1]+30), "PM"],
-				  					10: ["11", convertToTwoDigits(String(timeEarly[1])), "PM"],
-				  					11: ["11", String(timeEarly[1]+30), "PM"],
-				  					12: ["12", convertToTwoDigits(String(timeEarly[1])), "AM"],
-				  					13: ["12", String(timeEarly[1]+30), "AM"],
-				  					14: ["01", convertToTwoDigits(String(timeEarly[1])), "AM"],
-				  					15: ["01", String(timeEarly[1]+30), "AM"]
-				  				};
-
-				  				function convertToTwoDigits(string) {
-				  					if (string.length == 1) {
-				  						return "0" + string;
-				  					}
-				  					else {
-				  						return string;
-				  					}
-				  				}
+				  					0: ["06", timeEarly, "PM"],
+				  					1: ["06", String(parseInt(timeEarly)+30), "PM"],
+				  					2: ["07", timeEarly, "PM"],
+				  					3: ["07", String(parseInt(timeEarly)+30), "PM"],
+				  					4: ["08", timeEarly, "PM"],
+				  					5: ["08", String(parseInt(timeEarly)+30), "PM"],
+				  					6: ["09", timeEarly, "PM"],
+				  					7: ["09", String(parseInt(timeEarly)+30), "PM"],
+				  					8: ["10", timeEarly, "PM"],
+				  					9: ["10", String(parseInt(timeEarly)+30), "PM"],
+				  					10: ["11", timeEarly, "PM"],
+				  					11: ["11", String(parseInt(timeEarly)+30), "PM"],
+				  					12: ["12", timeEarly, "AM"],
+				  					13: ["12", String(parseInt(timeEarly)+30), "AM"],
+				  					14: ["01", timeEarly, "AM"],
+				  					15: ["01", String(parseInt(timeEarly)+30), "AM"]
+				  				};				  				
 
 				  				function setLateOrEarly() {
 				  					prob = Math.random(1);
 				  					if (prob < 0.5) {
-				  						return ["red", "late"];
+				  						return ["#ea666e", "late"];
 				  					}
 				  					else {
-				  						return ["green", "early"];
+				  						return ["#96ee82", "early"];
 				  					}
 				  				}
 				  				lateOrEarly = setLateOrEarly();
-				  				infoToDisplay += "<p class='userp'>Scheduled arrival time: " + dictOfTimes[randomTime][0] + ":" + dictOfTimes[randomTime][1] + " " + dictOfTimes[randomTime][2] + "</p>";
-				  				infoToDisplay += "<p class='userp " + lateOrEarly[0] + "'>Estimated time until arrival: " + String(Math.ceil(Math.random(1)*5)) + " min " + lateOrEarly[1] + "</p></div>";
-				  				console.log(infoToDisplay);
+				  				infoToDisplay += "<p>Scheduled arrival time: " + dictOfTimes[randomTime][0] + ":" + dictOfTimes[randomTime][1] + " " + dictOfTimes[randomTime][2] + "</p>";
+				  				infoToDisplay += "<p>Estimated time until arrival: <span style='color:" + lateOrEarly[0] + "'>" + String(Math.ceil(Math.random(1)*5)) + " min " + lateOrEarly[1] + "</span></p></div>";
 				  				$("#userInput").append(infoToDisplay);
 				  			}
 				  		}
@@ -126,14 +120,21 @@ $(document).ready(function() {
 		    		newVanStop.marker.addTo(map);	    		
 		    		campusVanStops.push(newVanStop);		    		
 		    	}
+		    	drawInitialSchedule(campusVanStops);		    	
 		    }
 		});
 	}
 
+	function drawInitialSchedule(stops) {
+		for (var i = 0; i < stops.length; i++) {
+			stop = stops[i];
+			scheduleToDisplay = "<tr>";
+			scheduleToDisplay += "<td>" + stop.title + "<br><em>" + stop.loc + "</em></td><td>" + stop.times[0][0] + ":" + stop.times[0][1] + " " + stop.times[0][2] + "</td><td>" + stop.times[1][0] + ":" + stop.times[1][1] + " " + stop.times[1][2] + "</td></tr>";
+			$("#schedule tbody").append(scheduleToDisplay);
+		}
+	}
 
-	//$("userInput").niceScroll({mousescrollstep:40, hidecursordelay: 200, scrollspeed: 40, cursorwidth: 7, cursorcolor: "#157195", cursoropacitymax: .7, cursoropacitymin: .3});
 	initialize();
 	drawCampus();
 	drawVanStops();
-
 });
